884d6a0850367737a1039863f56fa8f9d5f6cef5
Lucas Werkmeister <mail@lucaswerkmeister.de>
2014-11-06 13:33:38 +0100

    Implement lowlight
    For every token, generate a span with the token type as class and the
    token text as content. These spans can then be styled using CSS.

--- js/lowlight.js
+++ js/lowlight.js
@@ -1,4 +1,25 @@
 "use strict";
+
+/*
+  NOT a general purpose escaper! Only valid when *content* of a tag, not within an attribute.
+ */
+function escape(code) {
+    return code
+        .replace("&", "&amp;", "g")
+        .replace("<", "&lt;", "g")
+        .replace(">", "&gt;", "g");
+}
+
 function lowlight(lang, lexer, code) {
-    return "Hello, " + code + "!";
+    var ret = "";
+    var lex = lexer.CeylonLexer(lexer.StringCharacterStream(code));
+    var cur;
+    while ((cur = lex.nextToken()) != null) {
+        ret += "<span class=\"";
+        ret += cur.type.string;
+        ret += "\">";
+        ret += escape(cur.text);
+        ret += "</span>";
+    }
+    return ret;
 }
